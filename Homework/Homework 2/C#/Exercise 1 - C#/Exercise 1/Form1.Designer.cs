c7c4e858c9ce92229bd0d4e7702a45f123264dc7
TheTripol3 <23087102+TheTripol3@users.noreply.github.com>
2020-10-19 01:13:45 +0200

    Add files via upload

---
+++ Homework/Homework 2/C#/Exercise 1 - C#/Exercise 1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Exercise_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(383, 247);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(401, 34);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(367, 247);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Alignment = System.Drawing.StringAlignment.Center;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(14, 287);
+            this.chart1.Name = "chart1";
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            series1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.Percent30;
+            series1.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series1.BorderColor = System.Drawing.Color.Black;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Relative Frequency";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(754, 180);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Online Arithmetic Mean";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(590, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Distribution";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Book", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(583, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Developed by Andrea Tripoli";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(782, 501);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Exercise 1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Homework/Homework 2/C#/Exercise 1 - C#/Exercise 1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Random R = new Random();
+        public Double CurrentAvarage = 0;
+        public int countNumber = 0;
+
+        public SortedDictionary<int, frequencies> FrequencyDistribution = new SortedDictionary<int, frequencies>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.richTextBox1.AppendText("Exam".PadRight(10) + "Grade".PadRight(8) + "Current Mean" + Environment.NewLine);
+
+            this.chart1.ChartAreas["ChartArea1"].AxisX.MinorTickMark.Enabled = true;
+            this.chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+            this.chart1.ChartAreas["ChartArea1"].AxisX.IsLabelAutoFit = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //Online arithmetical mean
+            //----------------------------------------------------
+
+            //Increment the counter value
+            countNumber += 1;
+
+            //The Knuth formula
+            int grade = R.Next(18, 31);
+            CurrentAvarage = CurrentAvarage + (grade - CurrentAvarage) / countNumber;
+
+            string nameOfTheExam = "Exam " + countNumber;
+
+            this.richTextBox1.AppendText(nameOfTheExam.PadRight(10) +
+                                       grade.ToString().PadRight(8) + CurrentAvarage +
+                                       Environment.NewLine);
+
+
+
+
+            //---------------------------------------------------
+            //Distribution
+
+            if (FrequencyDistribution.ContainsKey(grade))
+            {
+                FrequencyDistribution[grade].countFrequencies += 1;
+            }
+            else
+            {
+                FrequencyDistribution.Add(grade, new frequencies());
+            }
+
+            //Clear every time
+            this.richTextBox2.Clear();
+            this.chart1.Series["Relative Frequency"].Points.Clear();
+            this.richTextBox2.AppendText("Grade".PadRight(7) + "Count".PadRight(7) + "Freq".PadRight(7) + "Perc".PadRight(7) + Environment.NewLine);
+
+            //Calculate the relative frequencies and percentage frequencies. Then draw the graphic
+            foreach (KeyValuePair<int, frequencies> freq in FrequencyDistribution)
+            {
+                FrequencyDistribution[freq.Key].RelativeFrequencies = Convert.ToDouble(FrequencyDistribution[freq.Key].countFrequencies) / Convert.ToDouble(countNumber);
+                FrequencyDistribution[freq.Key].PercentageFrequencies = Convert.ToDouble(FrequencyDistribution[freq.Key].RelativeFrequencies) * 100;
+
+                string fr = "0." + freq.Value.RelativeFrequencies.ToString("0.##");
+                this.chart1.Series["Relative Frequency"].Points.AddXY(freq.Key.ToString(), fr);
+
+                this.richTextBox2.AppendText(freq.Key.ToString().PadRight(7) +
+                                           freq.Value.countFrequencies.ToString().PadRight(7) +
+                                           freq.Value.RelativeFrequencies.ToString("0.##").PadRight(7) +
+                                           freq.Value.PercentageFrequencies.ToString("0.##").PadRight(7) + " % " + Environment.NewLine);
+
+
+
+            }
+
+            this.richTextBox2.AppendText(Environment.NewLine + Environment.NewLine +
+                                       "Total Count: " + countNumber + Environment.NewLine);
+
+
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.timer1.Start();
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.timer1.Stop();
+        }
+    }
+
+    // Class Frequencies
+    public class frequencies
+    {
+
+        public int countFrequencies = 1;
+        public Double RelativeFrequencies;
+        public Double PercentageFrequencies;
+
+    }
+}
