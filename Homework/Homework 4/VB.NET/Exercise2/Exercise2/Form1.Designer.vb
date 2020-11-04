663a8d59fa3d5c1609a67e6db284bd3e945966ed
TheTripol3 <23087102+TheTripol3@users.noreply.github.com>
2020-11-04 15:48:42 +0100

    Updatet Ex 2

---
+++ Homework/Homework 4/VB.NET/Exercise2/Exercise2/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 11)
+        Me.Label1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(30, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Setup"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(151, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Draw"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 129)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(547, 582)
+        Me.WebBrowser1.TabIndex = 4
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(598, 12)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(673, 86)
+        Me.RichTextBox2.TabIndex = 5
+        Me.RichTextBox2.Text = ""
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.PictureBox1.Location = New System.Drawing.Point(598, 134)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(673, 577)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 730)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1283, 41)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Lucida Console", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Menu
+        Me.Label2.Location = New System.Drawing.Point(501, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(306, 19)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Developed by Andrea Tripoli"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(1283, 771)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(1299, 810)
+        Me.MinimumSize = New System.Drawing.Size(1299, 810)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Homework/Homework 4/VB.NET/Exercise2/Exercise2/Form1.vb
@@ -0,0 +1,201 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+
+    Public LoadEvents As Integer
+
+    Public b As Bitmap
+    Public g As Graphics
+
+    Public ViewPort As New Rectangle(0, 0, 450, 450)
+    Public colorsArray() As Color = New Color() {Color.FromArgb(255, 0, 0), Color.FromArgb(0, 255, 0), Color.FromArgb(0, 0, 255)}
+    Dim R As New Random
+
+
+    Private Sub Button1_MouseClick(sender As Object, e As MouseEventArgs) Handles Button1.MouseClick
+        Dim url2 As String = "https://www.ilgiornale.it/"
+
+        WebBrowser1.ScriptErrorsSuppressed = True
+        WebBrowser1.Url = New Uri(url2)
+    End Sub
+
+    Private Sub Button2_MouseClick(sender As Object, e As MouseEventArgs) Handles Button2.MouseClick
+        Try
+            WebBrowser1.Document.ExecCommand("SelectAll", False, Nothing)
+            WebBrowser1.Document.ExecCommand("Copy", False, Nothing)
+            WebBrowser1.Document.ExecCommand("Unselect", False, Type.Missing)
+        Catch ex As Exception
+            MessageBox.Show("You have to click on the setup!")
+        End Try
+        Me.RichTextBox2.Text = Clipboard.GetText()
+        Clipboard.Clear()
+
+
+        Dim stopWords As New HashSet(Of String)(StringComparer.InvariantCultureIgnoreCase)
+        Dim FileParoleFiltrate As String = My.Resources.Resource1.StopList_Ita
+
+        Using s As New StringReader(FileParoleFiltrate)
+
+            Do
+                Dim Line As String = s.ReadLine
+                If Line Is Nothing Then Exit Do
+
+
+                Dim j As Integer = Line.IndexOf("|")
+                If j > -1 Then
+                    Line = Line.Substring(0, j).Trim
+                Else
+                    Line = Line.Trim
+                End If
+
+
+                If Not String.IsNullOrWhiteSpace(Line) Then
+                    stopWords.Add(Line)
+                End If
+
+            Loop
+
+        End Using
+
+
+        Dim Text As New Text.StringBuilder(RichTextBox2.Text)
+        Dim wordCount As Integer = 0, index As Integer = 0
+        Dim CurrentWord As New Text.StringBuilder
+        Dim wordsDistribution As New Dictionary(Of String, Integer)(StringComparer.InvariantCultureIgnoreCase)
+
+
+
+        'skip whitespace until first word
+        While index < Text.Length AndAlso Char.IsWhiteSpace(Text(index))
+            index += 1
+        End While
+
+
+
+
+        While index < Text.Length
+
+            While index < Text.Length AndAlso Not Char.IsWhiteSpace(Text(index))
+                CurrentWord.Append(Text(index))
+                index += 1
+            End While
+
+            Dim Parola As String = CurrentWord.ToString.Replace("?", "").Replace("!", "").Replace("|", "").Replace("-", "").Replace("*", "").Replace("(", "")
+
+            CurrentWord.Length = 0
+
+            If Not stopWords.Contains(Parola) Then
+
+                If Not wordsDistribution.ContainsKey(Parola) Then
+                    wordsDistribution.Add(Parola, 1)
+                Else
+                    wordsDistribution(Parola) += 1
+                End If
+            End If
+            wordCount += 1
+
+
+            ' skip whitespace until next word
+            While index < Text.Length AndAlso Char.IsWhiteSpace(Text(index))
+                index += 1
+            End While
+
+        End While
+
+
+        Me.InitializeGraphics()
+        Me.g.Clear(Color.White)
+
+
+        printWords(wordsDistribution)
+
+
+    End Sub
+
+
+
+
+
+
+    Private Sub printWords(dict As Dictionary(Of String, Integer))
+
+
+        Dim ListOfKV As List(Of KeyValuePair(Of String, Integer)) = dict.OrderByDescending(Function(p) p.Value).ToList
+
+        Dim ListaRect As New List(Of Rectangle)
+
+        For Each kvp As KeyValuePair(Of String, Integer) In ListOfKV
+            Dim TryRect As Rectangle
+            Dim f As New Font("Arial", kvp.Value * 2)
+            Dim s As Size = Size.Truncate(g.MeasureString(kvp.Key, f))
+
+            Dim Tries As Integer
+            Do
+                Dim x = R.Next(ViewPort.Left, ViewPort.Right + 1)
+                Dim y = R.Next(ViewPort.Top, ViewPort.Bottom + 1)
+
+                TryRect = New Rectangle(New Point(x, y), s)
+                If Not SpotIsAlreadyOccupied(TryRect, ListaRect) Then Exit Do
+                Tries += 1
+
+                If Tries > 1000 Then Continue For
+
+            Loop
+
+            g.DrawString(kvp.Key, f, New SolidBrush(GiveRandomColor), New Point(TryRect.X, TryRect.Y))
+            ListaRect.Add(TryRect)
+
+            If kvp.Value < 3 Then Exit For
+
+        Next
+        Me.PictureBox1.Image = b
+
+    End Sub
+
+
+
+
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        LoadEvents += 1
+        Me.Label1.Text = "Load" & LoadEvents
+    End Sub
+
+
+    Function SpotIsAlreadyOccupied(TargetSpot As Rectangle, ListaRect As List(Of Rectangle)) As Boolean
+
+        If ListaRect.Count = 0 Then Return False
+
+        For Each ExistingSpot As Rectangle In ListaRect
+
+            If ExistingSpot.IntersectsWith(TargetSpot) Then Return True
+            If ExistingSpot.IntersectsWith(TargetSpot) Then Return True
+            If TargetSpot.Contains(ExistingSpot) Then Return True
+        Next
+
+        Return False
+
+
+    End Function
+
+
+
+    Sub InitializeGraphics()
+        Me.b = New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Me.g = Graphics.FromImage(b)
+        Me.g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        Me.g.TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+    End Sub
+
+
+    Function GiveRandomColor() As Color
+
+        Dim colorsArray As Array = [Enum].GetValues(GetType(KnownColor))
+        Dim k As Integer = colorsArray(R.Next(0, colorsArray.Length))
+
+        Return Color.FromKnownColor(k)
+
+    End Function
+
+End Class
