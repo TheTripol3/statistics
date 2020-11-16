3a8440f9df2d80b08f6b8c98109157ba8b24c917
TheTripol3 <23087102+TheTripol3@users.noreply.github.com>
2020-11-16 16:45:26 +0100

    Insert

---
+++ Homework/Homework 6/VB.NET/RunningMedian/RunningMedian/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1500
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(832, 439)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 28)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 498)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(924, 41)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(285, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(355, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Developed by Andrea Tripoli"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.RichTextBox3)
+        Me.Panel2.Controls.Add(Me.RichTextBox2)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.RichTextBox1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(924, 498)
+        Me.Panel2.TabIndex = 5
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(420, 392)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(492, 12)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(420, 392)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.Location = New System.Drawing.Point(12, 421)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(797, 60)
+        Me.RichTextBox3.TabIndex = 2
+        Me.RichTextBox3.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(924, 539)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(940, 578)
+        Me.MinimumSize = New System.Drawing.Size(940, 578)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Running Median"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Homework/Homework 6/VB.NET/RunningMedian/RunningMedian/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+
+    Private R As New Random
+    Dim heaps As New heap
+
+    Public Class heap
+        Private listUnordered As New List(Of Integer)
+        Private listOrdered As New List(Of Integer)
+
+
+        Public Function getOrdinary() As List(Of Integer)
+            Return listOrdered
+        End Function
+
+        Public Function getNoOrdinary() As List(Of Integer)
+            Return listUnordered
+        End Function
+
+        Public Sub add(num As Integer)
+            listUnordered.Add(num)
+            listOrdered.Add(num)
+            listOrdered.Sort()
+        End Sub
+
+        Public Function calculateMedian() As Double
+            Dim res As Double = getMedian(listOrdered)
+            Return res
+        End Function
+
+        Public Function getMedian(list As List(Of Integer)) As Double
+            Dim idx As Integer = Nothing
+            Dim result As Double = Nothing
+            If (list.Count) Mod 2 = 0 Then
+                Dim idxUp As Integer = Math.Ceiling((list.Count - 1) / 2)
+                Dim idxDown As Integer = Math.Floor((list.Count - 1) / 2)
+
+                result = (list.ElementAt(idxUp) + list.ElementAt(idxDown)) / 2
+
+                Return result
+
+            Else
+                idx = (list.Count - 1) / 2
+                result = list.ElementAt(idx)
+                Return result
+            End If
+        End Function
+
+
+    End Class
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.RichTextBox2.Clear()
+        Me.RichTextBox3.Clear()
+        Me.RichTextBox2.AppendText("Ordered List" & Environment.NewLine & Environment.NewLine)
+
+        Dim number As Integer = R.Next(0, 100)
+        heaps.add(number)
+        Dim median As Double = heaps.calculateMedian()
+        Dim listNoOrdered As List(Of Integer) = heaps.getNoOrdinary
+        Dim listOrdered As List(Of Integer) = heaps.getOrdinary
+
+        Me.RichTextBox1.AppendText(number & Environment.NewLine)
+        Me.RichTextBox3.AppendText("Median  " & median & Environment.NewLine)
+
+        For Each kvp In listOrdered
+            Me.RichTextBox2.AppendText(kvp.ToString & Environment.NewLine)
+        Next
+
+    End Sub
+
+    Private Sub Button1_MouseClick(sender As Object, e As MouseEventArgs) Handles Button1.MouseClick
+        If (Timer1.Enabled) Then
+            Timer1.Stop()
+            Button1.Text = "Start"
+        Else
+            Timer1.Start()
+            Button1.Text = "Stop"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.RichTextBox1.AppendText("Unordered List" & Environment.NewLine & Environment.NewLine)
+    End Sub
+End Class
